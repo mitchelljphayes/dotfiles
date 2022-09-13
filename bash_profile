e66d4771af0f81bd73bc51d28cf9691316401a88
Mitch Hayes <mitchelljphayes@gmail.com>
2022-09-13 20:56:08 +1000

    first commit 🍾

---
+++ bash_profile
@@ -0,0 +1,4 @@
+source ~/.bashrc
+
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-15.0.2.jdk/Contents/Home
+. "$HOME/.cargo/env"
---
+++ nvim/lua/mitch/colorscheme.lua
@@ -0,0 +1,16 @@
+local colorscheme = "gruvbox"
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+--vim.cmd [[
+--try
+ -- colorscheme darkplus
+--catch /^Vim\%((\a\+)\)\=:E185/
+  --colorscheme default
+  --set background=dark
+--endtry
+--]]
