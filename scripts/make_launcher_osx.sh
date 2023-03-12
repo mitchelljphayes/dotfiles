4dbe970248bb5fe8cfaa57b0cd8df0206ebe60e2
mitchelljphayes <18720183+mitchelljphayes@users.noreply.github.com>
2023-03-13 07:34:32 +1100

    Delete scripts directory

--- scripts/make_launcher_osx.sh
+++
@@ -1,5 +0,0 @@
-read "?Enter your UNSW zID: " zid 
-mkdir $HOME/CSE
-printf "sshfs -o follow_symlinks,noappledouble,defer_permissions,local,volname=CSE $zid@login.cse.unsw.edu.au: $HOME/CSE\n" > $HOME/Desktop/connect_cse
-chmod +x $HOME/Desktop/connect_cse
-
