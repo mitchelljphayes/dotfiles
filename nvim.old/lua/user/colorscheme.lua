a67392b6f44d7e7d942190bfdbe56db0ce270754
Mitch Hayes <mitchelljphayes@gmail.com>
2023-06-29 14:07:13 +1000

    moved to kickstart nvim

---
+++ nvim.old/lua/user/colorscheme.lua
@@ -0,0 +1,21 @@
+local colorscheme = "ayu"
+
+-- require('ayu').setup({
+--     mirage = true, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
+--     overrides = {}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+-- })
+
+local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+if not status_ok then
+  vim.notify("colorscheme " .. colorscheme .. " not found!")
+  return
+end
+
+-- [[ vim.cmd [[ ]]
+-- [[   try ]]
+-- [[      colorscheme darkplus ]]
+-- [[     catch /^Vim\%((\a\+)\)\=:E185/ ]]
+-- [[     colorscheme default ]]
+-- [[     set background=dark ]]
+-- [[   end try ]]
+-- ]]
