e6a016ebe1609dcb4ef922ef4876bc02488395d7
Mitch Hayes <mitchelljphayes@gmail.com>
2023-09-17 19:16:54 +1000

    added support for nushell

--- bash/plugins.bash
+++ bash/plugins.bash
@@ -3,6 +3,8 @@ if [[ "$(tput colors)" == "256" ]]; then
     eval "$(dircolors ~/.shell/plugins/dircolors-solarized/dircolors.256dark)"
 fi
 
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
