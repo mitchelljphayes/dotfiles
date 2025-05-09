59599e1c0a593ecc2488af27c4ef6a6c3b000ab8
Mitch Hayes <mitchelljphayes@gmail.com>
2025-05-09 20:31:25 +1000

    updates to tmux conf and nvim colours

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,15 +1,33 @@
 return {
-  "catppuccin/nvim",
-  name = "catppuccin",
+  "navarasu/onedark.nvim",
+  name = "onedark",
   priority = 1000,
+  style = "darker",
   config = function()
-    vim.cmd.colorscheme 'catppuccin'
+    vim.cmd.colorscheme 'onedark'
   end,
 }
+-- return {
+--   "catppuccin/nvim",
+--   name = "catppuccin",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme 'catppuccin'
+--   end,
+-- }
 -- {
 --   'Shatur/neovim-ayu',
 --   priority = 1000,
 --   config = function()
 --     vim.cmd.colorscheme 'ayu-mirage'
+--   end,
+-- }
+-- 
+-- return {
+--   "EdenEast/nightfox.nvim",
+--   name = "carbonfox",
+--   priority = 1000,
+--   config = function()
+--     vim.cmd.colorscheme 'carbonfox'
 --   end,
 -- }
