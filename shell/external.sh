f14a5b54221a7d3b5a4608e8a68df955c53757db
Mitch Hayes <mitchelljphayes@gmail.com>
2025-12-27 13:28:41 +1000

    chore(shell): update bash and zsh configurations
    - Remove deprecated shell scripts (bootstrap.sh, env.sh, external.sh)
    - Simplify zsh prompt configuration
    - Update aliases and functions
    - Clean up path configuration
    - Add zsh-autosuggestions to plugins

--- shell/external.sh
+++
@@ -1,4 +0,0 @@
-# Environment variables have been moved to shell/environment.sh
-# This file is kept for backwards compatibility and can be used for
-# external tool configurations that don't fit in environment.sh
-
