e3fd245445d31883896f11ea9602c816227b013b
Mitch Hayes <mitchelljphayes@gmail.com>
2026-03-05 22:16:10 +1100

    Add inline image viewing and PDF opening from Neovim
    - Configure image.nvim to load eagerly with hijack_file_patterns for
      inline rendering of images (png, jpg, gif, etc.) via Kitty protocol
    - Add media-viewer plugin to open PDFs/ePubs in system viewer
    - Use magick_cli processor with ImageMagick for image processing

--- nvim/lua/plugins/molten.lua
+++ nvim/lua/plugins/molten.lua
@@ -5,22 +5,25 @@ return {
         dependencies = { "3rd/image.nvim" },
         build = ":UpdateRemotePlugins",
         init = function()
-            -- these are examples, not defaults. Please see the readme
             vim.g.molten_image_provider = "image.nvim"
             vim.g.molten_output_win_max_height = 20
         end,
     },
     {
-        -- see the image.nvim readme for more information about configuring this plugin
         "3rd/image.nvim",
+        build = false, -- don't build the rock, use magick_cli
+        lazy = false,  -- must load eagerly for hijack_file_patterns to work
         opts = {
-            backend = "kitty", -- whatever backend you would like to use
-            max_width = 100,
-            max_height = 12,
-            max_height_window_percentage = math.huge,
-            max_width_window_percentage = math.huge,
-            window_overlap_clear_enabled = true, -- toggles images when windows are overlapped
+            backend = "kitty",
+            processor = "magick_cli",
+            max_width_window_percentage = 80,
+            max_height_window_percentage = 80,
+            window_overlap_clear_enabled = true,
             window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+            -- Render image files inline when opened in nvim
+            hijack_file_patterns = { "*.png", "*.jpg", "*.jpeg", "*.gif", "*.webp", "*.avif", "*.bmp", "*.ico", "*.svg" },
+            -- tmux support
+            tmux_show_only_in_active_window = true,
         },
-    }
+    },
 }
