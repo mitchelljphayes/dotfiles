876dfb251c2e431fbf69df77ec1fe87ef5473a69
Mitch Hayes <mitchelljphayes@gmail.com>
2026-03-21 22:11:29 +1100

    Fix lazy.nvim bootstrap failing on fresh machines

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,7 @@
--- Install package manager
--- https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
 -- Bootstrap lazy.nvim
-local
-lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.fn.isdirectory(lazypath) then
+-- https://github.com/folke/lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
   local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
